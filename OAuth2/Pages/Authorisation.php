084619f771df80959ee6c5c0a7174f6a752b1bd8
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-17 22:05:17 +0000

    Authorisation code generation

--- OAuth2/Pages/Authorisation.php
+++ OAuth2/Pages/Authorisation.php
@@ -1,16 +1,81 @@
 <?php
 
-    namespace IdnoPlugins\OAuth2\Pages {
+namespace IdnoPlugins\OAuth2\Pages {
 
-        class Authorisation extends \Idno\Common\Page {
+    class Authorisation extends \Idno\Common\Page {
 
-            function getContent() {
+	function getContent() {
 
-             
-            }
+	    try {
+		try {
+		    $state = $this->getInput('state');
+		    $scope = $this->getInput('scope');
+		    $response_type = $this->getInput('response_type');
+		    $client_id = $this->getInput('client_id');
+		    $redirect_uri = $this->getInput('redirect_uri');
+		    
+		    if (!$response_type) throw new \IdnoPlugins\OAuth2\OAuth2Exception("Required parameter response_type is missing!", 'invalid_request', $state);
+		    if (!$client_id) throw new \IdnoPlugins\OAuth2\OAuth2Exception("Required parameter client_id is missing!", 'invalid_request', $state);
+		    
+		    switch ($response_type) {
+			
+			case 'code':
+			default:
+			    // Generate code
+			    $code = new \IdnoPlugins\OAuth2\Code();
+			    
+			    // Save context
+			    $code->scope = $scope;
+			    $code->client_id = $client_id;
+			    $code->state = $state;
+			    
+			    // Check code 
+			    if ($code->getOne(['code' => $code]))
+				throw new \IdnoPlugins\OAuth2\OAuth2Exception("Sorry, this code has been seen before!", 'access_denied', $state);
+			    
+			    // Save code so we've not seen it before
+			    if (!$code->save()) throw new \IdnoPlugins\OAuth2\OAuth2Exception("Bang, code incorrect", 'invalid_request', $state);
+			    
+			    // Forward or echo
+			    if ($redirect_uri) {
+				
+				// Normalise url and add parameters
+				if (strpos($redirect_uri, '?')===false)
+					$redirect_uri .= '?';
+				$redirect_uri .= 'code=' . urlencode ($code) . '&state=' . urlencode($state);
+				
+				// Forward
+				$this->forward($redirect_uri);
+				
+			    } else {
+				// Otherwise echo result
+				echo json_encode([
+				    'code' => $code,
+				    'state' => $state
+				]);
+			    }
+		    }
+		    
+		    
+		} catch (\IdnoPlugins\OAuth2\OAuth2Exception $oa2e) {
+		    $this->setResponse($oa2e->http_code);
+		    echo $oa2e->jsonSerialize();
+		}
+		
+	    } catch (\Exception $e) {
+		$this->setResponse(400);
+		
+		echo json_encode([
+		    'error' => 'invalid_request',
+		    'error_description' => $e->getMessage()
+		]);
+	    }
+	}
 
-            function postContent() { $this->getContent(); }
-
-        }
+	function postContent() {
+	    $this->getContent();
+	}
 
     }
+
+}
