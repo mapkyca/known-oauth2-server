fbc2c7a482f4903e1dee1c0f4d354829c278a6fa
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-17 18:55:55 +0000

    Stub server

---
+++ OAuth2/Server.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace IdnoPlugins\OAuth2 {
+
+    /**
+     * OAuth2 Server.
+     */
+    class Server {
+	
+	/**
+	 * Generate a new api key + secret key.
+	 * 
+	 * @return array or 'key' and 'secret'
+	 */
+	public function generateKeypair() {
+	    
+	}
+    }
+
+}
