d9b3618e8839d8dbd9ee707a837687787a1c9084
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-17 18:56:21 +0000

    Stub endpoint pages

---
+++ OAuth2/Pages/Authorisation.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace IdnoPlugins\OAuth2\Pages {
+
+        class Authorisation extends \Idno\Common\Page {
+
+            function getContent() {
+
+             
+            }
+
+            function postContent() { $this->getContent(); }
+
+        }
+
+    }
