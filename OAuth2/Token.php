f7943cb126f5a7acc1567c735648145779036cb4
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-18 09:54:50 +0000

    Token object

---
+++ OAuth2/Token.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace IdnoPlugins\OAuth2 {
+
+
+    class Token extends \Idno\Common\Entity {
+	
+	function __construct($token_type = 'grant', $expires_in = 2419200){ 
+	    
+	    parent::__construct();
+	    
+	    $this->access_token = hash('sha256', mt_rand() . microtime(true));
+	    $this->refresh_token = hash('sha256', mt_rand() . microtime(true));
+	    $this->expires_in = $expires_in; // Default expires is 1 month, like facebook
+	    $this->token_type = $token_type;
+	    
+	    $this->setTitle($this->access_token); // better stub generation, not that it matters
+	}
+
+	/**
+	 * Saves changes to this object based on user input
+	 * @return true|false
+	 */
+	function saveDataFromInput() {
+
+	    if (empty($this->_id)) {
+		$new = true;
+	    } else {
+		$new = false;
+	    }
+
+	    if ($time = \Idno\Core\site()->currentPage()->getInput('created')) {
+		if ($time = strtotime($time)) {
+		    $this->created = $time;
+		}
+	    }
+
+	    $this->setAccess('PUBLIC');
+	    return $this->save();
+	}
+	
+	function jsonSerialize() { // Code is only ever serialised as part of something else
+	    return [
+		'access_token' => $this->access_token,
+		'refresh_token' => $this->refresh_token,
+		'expires_in' => $this->expires_in,
+		'token_Type' => $this->token_type
+	    ];
+	}
+	
+
+    }
+
+}
