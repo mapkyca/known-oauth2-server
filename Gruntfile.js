0733767c9089c4ce1381f127288a51ad1ca63187
Marcus Povey <marcus@marcus-povey.co.uk>
2019-08-04 09:29:57 +0100

    Updating language file, correcting Gruntfile for new location

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,31 @@
 /**
- * Gruntfile for OAuth2
+ * Sample language Gruntfile.
+ * 
+ * Copy this to your Known plugin root, rename to Gruntfile.js, and create a package.json 
+ * with an appropriate "name" variable (usually your package namespace).
  */
 
 module.exports = function (grunt) {
+    
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	pkg: grunt.file.readJSON('package.json'),
     });
 
-    // Build language pack (todo: find a cleaner way)
+    // Build your language file
     grunt.registerTask('build-lang', '', function(){
-
-        const { execSync } = require('child_process');
-
-        execSync('touch ./languages/oauth2.pot'); // Make sure it exists, if we're going to remove (for broken builds)
-        execSync('rm ./languages/oauth2.pot'); // Remove existing
-
-        execSync('find . -type f -regex ".*\.php" | php ../../languages/processfile.php >> ./languages/oauth2.pot');
-
+	
+	const { execSync } = require('child_process');
+	
+	var pot = grunt.config.get('pkg.name').toLowerCase() + '.pot';
+	
+	console.log("Building language file as ./languages/" + pot);
+	
+	execSync('touch ./languages/' + pot); // Make sure it exists, if we're going to remove (for broken builds)
+	execSync('rm ./languages/' + pot); // Remove existing
+	
+	execSync('find . -type f -regex ".*\.php" | php vendor/mapkyca/known-language-tools/buildpot.php >> ./languages/' + pot); 
+	
     });
 
 };
