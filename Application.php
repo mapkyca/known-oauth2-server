33e955021c5113c2ac01444565410d07b67c1d9d
Marcus Povey <marcus@instruct-eric.eu>
2020-05-02 18:26:13 +0100

    Generate a public / private key pair for new application

--- Application.php
+++ Application.php
@@ -25,6 +25,7 @@ namespace IdnoPlugins\OAuth2 {
             $app = new Application();
             $app->setTitle($title);
             $app->generateKeypair();
+            $app->generateAsymetricKeypair(); 
 
             return $app;
         }
@@ -49,9 +50,24 @@ namespace IdnoPlugins\OAuth2 {
          * Generate a new public / private key pair suitable for asymetric OIDC tokens
          */
         protected function generateAsymetricKeypair() {
+            $config = array(
+                "digest_alg" => "sha256",
+                "private_key_bits" => 4096,
+                "private_key_type" => OPENSSL_KEYTYPE_RSA,
+            );
+
+            // Create the private and public key
+            $res = openssl_pkey_new($config);
+
+            // Extract the private key from $res to $privKey
+            openssl_pkey_export($res, $privKey);
+
+            // Extract the public key from $res to $pubKey
+            $pubKey = openssl_pkey_get_details($res);
+            $pubKey = $pubKey["key"];
             
-            // TODO
-            
+            $this->publickey = $pubKey;
+            $this->privatekey = $privKey;
         }
         
         /**
